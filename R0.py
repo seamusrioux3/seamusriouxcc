4168c3bcacd2c780ceda9fc707f1ff64eea6c016
seamusrioux3 <seamus.rioux3@gmail.com>
2021-01-26 10:46:53 -0500

    Completed Task #1 - Added R0 Types

---
+++ R0.py
@@ -0,0 +1,16 @@
+class RNum:
+    def __init__(self, _num):
+        num = _num
+
+class RNegate:
+    def __init__(self,_num):
+        num = _num
+
+class RAdd:
+    def __init__(self, _left, _right):
+        left =_left
+        right = _right
+
+class RRead:
+    def __init__(self):
+        print("Not implemented")
