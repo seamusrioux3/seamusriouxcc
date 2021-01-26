569bfbfa155cb0b5167f5f666c2aba42e3378cd6
seamusrioux3 <seamus.rioux3@gmail.com>
2021-01-26 11:01:42 -0500

    Completed Task #2 - Added pp function

--- R0.py
+++ R0.py
@@ -1,16 +1,32 @@
 class RNum:
     def __init__(self, _num):
-        num = _num
+        self.num = _num
+
+    def pp(self):
+        return str(self.num)
 
 class RNegate:
     def __init__(self,_num):
-        num = _num
+        self.num = _num
+    
+    def pp(self):
+        return "-(" + str(self.num.pp()) + ")"
 
 class RAdd:
     def __init__(self, _left, _right):
-        left =_left
-        right = _right
+        self.left =_left
+        self.right = _right
+    
+    def pp(self):
+        return "(+ " + self.left.pp() +" " + self.right.pp() + ")"
 
 class RRead:
     def __init__(self):
         print("Not implemented")
+
+
+print(RNum(5).pp())
+print(RNegate(RNum(5)).pp())
+print(RAdd(RNum(5),RNum(6)).pp())
+print(RAdd(RNegate(RNum(5)),RNum(6)).pp())
+print(RAdd(RAdd(RNum(5),RNum(6)),RNum(6)).pp())
