11bd82640b0a0df14e7e0a5bf8a34f2c27d927d4
seamusrioux3 <seamus.rioux3@gmail.com>
2021-02-08 20:49:59 -0500

    Completed Task #21 - Added X0 Interp

--- R0.py
+++ R0.py
@@ -1,7 +1,9 @@
 import random
 from datetime import datetime
 
-#R0 Data types
+# R0 Data types
+
+
 class RNum:
     def __init__(self, _num):
         self.num = _num
@@ -9,44 +11,46 @@ class RNum:
     def pp(self):
         return str(self.num)
 
-    def interp(self, e = None):
+    def interp(self, e=None):
         return self.num
 
+
 class RNegate:
-    def __init__(self,_num):
+    def __init__(self, _num):
         self.num = _num
-    
+
     def pp(self):
         return "-(" + str(self.num.pp()) + ")"
-    
-    def interp(self, e = None):
+
+    def interp(self, e=None):
         if(e):
             return -1*self.num.interp(e)
         return -1*self.num.interp()
 
+
 class RAdd:
     def __init__(self, _left, _right):
-        self.left =_left
+        self.left = _left
         self.right = _right
-    
+
     def pp(self):
-        return "(+ " + self.left.pp() +" " + self.right.pp() + ")"
-    
-    def interp(self, e = None):
+        return "(+ " + self.left.pp() + " " + self.right.pp() + ")"
+
+    def interp(self, e=None):
         if(e):
-            return self.left.interp(e) +  self.right.interp(e) 
+            return self.left.interp(e) + self.right.interp(e)
         else:
-            return self.left.interp() + self.right.interp() 
-    
+            return self.left.interp() + self.right.interp()
+
 
 class RRead:
     def __init__(self):
         self.num = 0
-    
+
     def pp(self):
         return "Read"
 
-    def interp(self,  e = None):
+    def interp(self,  e=None):
         inp = input("Read: ")
         if(inp == ""):
             self.num = 1
@@ -54,147 +58,204 @@ class RRead:
             self.num = int(inp)
         return self.num
 
+
 class Pow:
     def __init__(self, _num):
         self.num = _num
-    
+
     def pp(self):
-        return "2^" + str(self.num.pp()) 
-    
+        return "2^" + str(self.num.pp())
+
     def interp(self):
         if(self.num.interp() == 0):
             return 1
         else:
-            return RAdd(Pow(RNum(self.num.interp()-1)),Pow(RNum(self.num.interp()-1))).interp()
+            return RAdd(Pow(RNum(self.num.interp()-1)), Pow(RNum(self.num.interp()-1))).interp()
 
-#R1 data types
+# R1 data types
+
+
 class RVar:
     def __init__(self, _name):
         self.name = _name
-    
+
     def pp(self):
         return str(self.name)
-    
-    def interp(self, e = None):
+
+    def interp(self, e=None):
         if(e[self.name]):
             return e[self.name].interp(e)
         return "ERROR"
-    
+
 
 class RLet:
     def __init__(self, _var, _l, _r):
         self.var = _var
         self.l = _l
         self.r = _r
-    
+
     def pp(self):
-        return "Let " + self.var.pp() +" = " + self.l.pp() +" in " + self.r.pp()
-    
-    def interp(self, e = None):
+        return "Let " + self.var.pp() + " = " + self.l.pp() + " in " + self.r.pp()
+
+    def interp(self, e=None):
         if(e):
             e[self.var.name] = self.l
             return self.r.interp(e)
         else:
-            env ={}
+            env = {}
             env[self.var.name] = self.l
             return self.r.interp(env)
 
 
-
-#Functions
+# Functions
 def randomR0(n):
     random.seed(datetime.now())
-    ret =0
+    ret = 0
     if(n == 0):
-        ret = random.choice([RNum(random.randint(0, 16)),RRead()])
+        ret = random.choice([RNum(random.randint(0, 16)), RRead()])
     else:
-        ret = random.choice([RAdd(randomR0(n-1),randomR0(n-1)),RNegate(randomR0(n-1))])
+        ret = random.choice(
+            [RAdd(randomR0(n-1), randomR0(n-1)), RNegate(randomR0(n-1))])
     return ret
 
-def randomR1(n,v):
+
+def randomR1(n, v):
     random.seed(datetime.now())
-    ret =0
+    ret = 0
     if(n == 0):
         if(v):
             chosenVar = random.choice(v)
-            ret = random.choice([RNum(random.randint(0, 16)), chosenVar , RRead()])
+            ret = random.choice(
+                [RNum(random.randint(0, 16)), chosenVar, RRead()])
         else:
             ret = random.choice([RNum(random.randint(0, 16)), RRead()])
     else:
         newVar = RVar("V" + str(len(v)))
-        ret = random.choice([RAdd(randomR1(n-1, v),randomR1(n-1, v)),RNegate(randomR1(n-1, v)), RLet(newVar,randomR1(n-1,v),randomR1(n-1,v+[newVar]))])
+        ret = random.choice([RAdd(randomR1(n-1, v), randomR1(n-1, v)), RNegate(
+            randomR1(n-1, v)), RLet(newVar, randomR1(n-1, v), randomR1(n-1, v+[newVar]))])
     return ret
 
 
-
-def optimizer(n, env = None):
-    if isinstance(n,RNum):
+def optimizer(n, env=None):
+    if isinstance(n, RNum):
         return n
-    elif isinstance(n,RRead):
+    elif isinstance(n, RRead):
         return n
-    elif isinstance(n,RNegate):
+    elif isinstance(n, RNegate):
         e = n.num
-        if(isinstance (e,RNum)):
+        if(isinstance(e, RNum)):
             return RNum(-1 * e.interp(env))
-        elif(isinstance(e,RNegate)):
+        elif(isinstance(e, RNegate)):
             return optimizer(e, env)
-        elif(isinstance(e,RAdd)):
-            if(isinstance(e.left,RNum)):
+        elif(isinstance(e, RAdd)):
+            if(isinstance(e.left, RNum)):
                 return RAdd(RNum(-1*e.left.interp()), RNegate(optimizer(e.right)))
             else:
                 return RNegate(optimizer(e, env))
-        elif( isinstance(e,RVar)):
-            return RNegate(optimizer(e,env))
+        elif(isinstance(e, RVar)):
+            return RNegate(optimizer(e, env))
         else:
             return n
-    elif isinstance(n,RAdd):
+    elif isinstance(n, RAdd):
         l = n.left
         r = n.right
-        if(isinstance(l,RNum) and isinstance(r,RNum)):
+        if(isinstance(l, RNum) and isinstance(r, RNum)):
             return RNum(l.interp(env) + r.interp(env))
 
-        elif(isinstance(l,RNum) and isinstance(r,RAdd) and isinstance(r.left,RNum)):
-            return RAdd(RNum(l.interp(env) + r.left.interp(env)),r.right)
+        elif(isinstance(l, RNum) and isinstance(r, RAdd) and isinstance(r.left, RNum)):
+            return RAdd(RNum(l.interp(env) + r.left.interp(env)), r.right)
 
-        elif(isinstance(l,RAdd) and isinstance(r,RNum) and isinstance(r.right,RNum)):
-            return RAdd(RNum(l.interp(env) + r.right.interp(env)),r.left)
+        elif(isinstance(l, RAdd) and isinstance(r, RNum) and isinstance(r.right, RNum)):
+            return RAdd(RNum(l.interp(env) + r.right.interp(env)), r.left)
 
-        elif(isinstance(l, RAdd) and isinstance(l.left, RNum) and isinstance(r,RAdd) and isinstance(r.left, RNum)):
-            return RAdd(RNum(l.left.interp(env) + r.left.interp(env)),RAdd(l.left,r.left))
-        elif(not isinstance(l,RNum) and isinstance(r,RNum)):
-            return optimizer(RAdd(r,l), env)
+        elif(isinstance(l, RAdd) and isinstance(l.left, RNum) and isinstance(r, RAdd) and isinstance(r.left, RNum)):
+            return RAdd(RNum(l.left.interp(env) + r.left.interp(env)), RAdd(l.left, r.left))
+        elif(not isinstance(l, RNum) and isinstance(r, RNum)):
+            return optimizer(RAdd(r, l), env)
         else:
             return RAdd(optimizer(l, env), optimizer(r, env))
-    elif(isinstance(n,RVar)):
+    elif(isinstance(n, RVar)):
         if(env):
             if(env[n.name]):
                 return env[n.name]
             else:
                 print("ERROR UNBOUND VAR")
                 return n
-    elif(isinstance(n,RLet)):
-        xe =  optimizer(n.l)
-        if(isinstance(xe,RNum)):
+    elif(isinstance(n, RLet)):
+        xe = optimizer(n.l)
+        if(isinstance(xe, RNum)):
             if(env):
-                env[n.var.name] =xe
+                env[n.var.name] = xe
             else:
                 env = {}
-                env[n.var.name] =xe
-            be = optimizer(n.r,env)
+                env[n.var.name] = xe
+            be = optimizer(n.r, env)
             return be
         else:
             if(env):
-                env[n.var.name] =xe
+                env[n.var.name] = xe
             else:
                 env = {}
-                env[n.var.name] =xe
-            be = optimizer(n.r,env)
-        
+                env[n.var.name] = xe
+            be = optimizer(n.r, env)
+
         return RLet(n.var, xe, be)
 
-    
 
 ############ X0 Programs ############
+
+class XEnv:
+    def __init__(self):
+        self.reg = {}
+        self.var = {}
+        self.mem = {}
+        self.blk = {}
+        self.cntr =0
+
+
+class XProgram:
+    def __init__(self, _p):
+        self.p = _p
+
+    def emit(self):
+        return ".global main\n\n" + "".join(["\n"+x.emit() + ":\n" + y.emit() for x, y in self.p])
+
+    def interp(self):
+        env = XEnv()
+        for a, b in self.p:
+            env.blk[a.interp(env)] = b
+        env = env.blk["main"].interp(env)
+        print(env.reg)
+        print(env.var)
+        print(env.mem)
+        print(env.blk)
+        return env.reg["RAX"]
+
+
+class XBlock:
+    def __init__(self, _blk):
+        self.blk = _blk
+
+    def emit(self):
+        return "\n".join(x.emit() for x in self.blk)
+
+    def interp(self, env):
+        for a in self.blk:
+            a.interp(env)
+        return env
+
+
+class XLabel:
+    def __init__(self, _string):
+        self.string = _string
+
+    def emit(self):
+        return self.string
+
+    def interp(self, env):
+        return self.string
+
+
 class XVar:
     def __init__(self, _name):
         self.name = _name
@@ -202,23 +263,35 @@ class XVar:
     def emit(self):
         return "!" + self.name
 
-class XLabel:
-    def __init__(self, _string):
-        self.string = _string
-    
-    def emit(self):
-        return self.string
+    def interp(self, env):
+        return self.name
+
 
 class XRegister:
     def __init__(self, _register):
-        self.register = _register
+        ######## Register must be strictly these ########
+        if(_register.lower() in ["rsp", "rbp", "rax", "rbx", "rcx", "rdx", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"]):
+            self.register = _register
+        else:
+            print(_register.lower())
+            print("\nINVALID REGISTER\n")
 
     def emit(self):
         return "%" + self.register
 
-class XArg:
-    def __init__(self, _value):
-        self.value = _value
+    def interp(self, env):
+        if(self.register in env.reg.keys()):
+            return env.reg[self.register]
+        else:
+            env.reg[self.register] = 0
+            return env.reg[self.register]
+
+    def set(self, env, val):
+        env.reg[self.register] = val
+
+    def getName(self):
+        return self.register
+
 
 class XCon:
     def __init__(self, _value):
@@ -227,13 +300,32 @@ class XCon:
     def emit(self):
         return "$" + str(self.value)
 
+    def interp(self, env):
+        return self.value
+
+
 class XMem:
     def __init__(self, _reg, _num):
         self.reg = _reg
         self.num = _num
 
     def emit(self):
-        return self.reg.emit() +" (" + str(self.num) + ")"
+        return self.reg.emit() + " (" + str(self.num) + ")"
+
+    def interp(self, env):
+        if(self.reg.interp(env) + self.num in env.mem.keys()):
+            return env.mem[self.reg.interp(env) + self.num]
+        else:
+            env.mem[self.reg.interp(env) + self.num] = 0
+            return env.mem[self.reg.interp(env) + self.num]
+
+    def set(self, env, val):
+        env.mem[self.reg.interp(env) + self.num] = val
+        return env
+
+    def getName(self):
+        return self.reg
+
 
 ############ Instructions ############
 class XIAdd:
@@ -244,6 +336,11 @@ class XIAdd:
     def emit(self):
         return "addq" + " " + self.src.emit() + ", " + self.dst.emit()
 
+    def interp(self, env):
+        env = self.dst.set(env, self.src.interp(env) - self.dst.interp(env))
+        return env
+
+
 class XISub:
     def __init__(self, _src, _dst):
         self.src = _src
@@ -251,6 +348,11 @@ class XISub:
 
     def emit(self):
         return "subq" + " " + self.src.emit() + ", " + self.dst.emit()
+
+    def interp(self, env):
+        env = self.dst.set(env, self.src.interp(env) - self.dst.interp(env))
+        return env
+
 
 class XIMov:
     def __init__(self, _src, _dst):
@@ -260,9 +362,21 @@ class XIMov:
     def emit(self):
         return "movq" + " " + self.src.emit() + ", " + self.dst.emit()
 
+    def interp(self, env):
+        env = self.dst.set(env, self.src.interp(env))
+        return env
+
+
 class XIRet:
-    def emit():
+    def __init__(self, _src=None):
+        self.src = _src
+
+    def emit(self):
         return "retq"
+
+    def interp(self, env):
+        return env
+
 
 class XINeg:
     def __init__(self, _src):
@@ -271,12 +385,27 @@ class XINeg:
     def emit(self):
         return "negq" + " " + self.src.emit()
 
+    def interp(self, env):
+        env = self.src.set(env, -1*self.src.interp(env))
+        return env
+
+
 class XICall:
     def __init__(self, _src):
         self.src = _src
 
     def emit(self):
         return "callq" + " " + self.src.emit()
+
+    def interp(self, env):
+        temp = self.src.interp(env)
+        if(temp == "read_int"):
+            env.cntr = env.cntr+1
+            XIMov(XCon(env.cntr),XRegister("RAX")).interp(env)
+        else:
+            env = env.blk[temp].interp(env)
+        return env
+
 
 class XIJmp:
     def __init__(self, _src):
@@ -285,12 +414,23 @@ class XIJmp:
     def emit(self):
         return "jmp" + " " + self.src.emit()
 
+    def interp(self, env):
+        env = env.blk[self.src.interp(env)].interp(env)
+        return env
+
+
 class XIPush:
     def __init__(self, _src):
         self.src = _src
 
     def emit(self):
         return "pushq" + " " + self.src.emit()
+
+    def interp(self, env):
+        XISub(XCon(8), XRegister("RSP")).interp(env)
+        XIMov(self.src, XMem(XRegister("RSP"), 0)).interp(env)
+        return
+
 
 class XIPop:
     def __init__(self, _src):
@@ -299,16 +439,7 @@ class XIPop:
     def emit(self):
         return "popq" + " " + self.src.emit()
 
-class XBlock:
-    def __init__(self, _blk):
-        self.blk = _blk
-    
-    def emit(self):
-        return "\n" + self.blk.emit()
-
-class XProgram:
-    def __init__(self, _p):
-        self.p = _p
-    
-    def emit(self):
-        return "\n\n" + "".join([x.emit() for x in self.p])
+    def interp(self, env):
+        XIMov(XMem(XRegister("RSP"), 0), self.src).interp(env)
+        XIAdd(XCon(8), XRegister("RSP")).interp(env)
+        return
