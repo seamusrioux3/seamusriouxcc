601f0ad21e965c9e21d6d1b33192c8c10f2ac636
Seamus Rioux <seamus.rioux3@gmail.com>
2021-02-15 02:30:47 -0500

    Completed Task #48 - Added runtime language

--- runtime.c
+++ runtime.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#define DEBUG  1
 
 static int read_count =0;
+int read_int(void);
+void print_int(int);
 
 int read_int(){
-    return read_count+=1;
+    int x =0;
+    if(DEBUG){
+        return 1;
+    }
+    scanf("%d",&x);
+    return x;
+    
+}
+
+void print_int(int x){
+    printf("%d", x);
 }
